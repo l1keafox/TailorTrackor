4b9b9a993f46b5e58d38c751151ea11c0c05f90f
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-03-10 11:58:41 -0700

    client and server running, response is not valid json

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,8 +2,21 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import client from "./../apollo-client"
 import { QUERY_USERS } from "./../utils/queries"
+import { useEffect } from 'react';
 export default function Home() {
-  console.log(client,QUERY_USERS);
+  
+  useEffect(()=>{
+    async function doCall (){
+      try{
+        const {data,error} = await client.query({query:QUERY_USERS})
+        console.log(error,data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    doCall();
+  },[] )
+  
   return (
     <>
       <Head>
