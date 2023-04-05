c54586827abf68a92fad8e67fd624b1ac237d56f
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-04-05 10:35:32 -0600

    up3

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 console.log(process.env.PGPASSWORD)
 
-const pool = new Pool(process.env.DATABASE_URL ?? {
+const pool = new Pool({
   host: process.env.PGHOST ?? "localhost",
   database:   process.env.PGDATABASE ?? "trackor",
   user:  process.env.PGUSER ?? "postgres",
